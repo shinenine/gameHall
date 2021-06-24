013cc8a69c7dca7e821f20bcac1d61f45e2fb1d0
shinenine <1205748010@qq.com>
2021-06-24 21:42:26 +0800

    finish

--- src/view/home.vue
+++ src/view/home.vue
@@ -41,12 +41,8 @@
     </div>
     <div class="bottomTabs" swipeable>
       <van-tabbar>
-        <van-tabbar-item replace to="/home" name="home" icon="home-o"
-          >首页</van-tabbar-item
-        >
-        <van-tabbar-item replace to="/games" name="setting" icon="setting-o"
-          >游戏</van-tabbar-item
-        >
+        <van-tabbar-item replace to="/home" name="home" icon="home-o">首页</van-tabbar-item>
+        <van-tabbar-item replace to="/games" name="setting" icon="setting-o">游戏</van-tabbar-item>
       </van-tabbar>
     </div>
   </div>
@@ -54,141 +50,141 @@
 
 <script src="../js/vueTouch.js"></script>
 <script>
-import card from "@/components/card.vue";
-import navBar from "@/components/navBar.vue";
-export default {
-  name: "home",
-  components: { navBar, card },
-  data() {
-    return {
-      value: "",
-      pictures: [
-        // 本地路径需要用require包裹
-        {
-          id: 1,
-          gameUrl: "",
-          url: require("../assets/swipe1.png")
-        },
-        {
-          id: 2,
-          url: require("../assets/swipe2.png")
-        },
-        {
-          id: 3,
-          url: require("../assets/swipe3.png")
-        },
-        {
-          id: 4,
-          url: require("../assets/swipe4.png")
-        }
-      ],
-      hottest: [
-        {
-          id: 1,
-          url: require("../assets/angryBird.jpg"),
-          name: "3D愤怒的小鸟",
-          gameType: "最受欢迎的经典益智弹弓小鸟游戏回来了！",
-          author: "Ultrasty",
-          gameUrl: "#",
-          gameDescription:
-            "愤怒小鸟的老玩家？愤怒小鸟经典游戏，你热爱的一切，都还在！而且增添了3D元素。选择要弹出的小鸟，与好友一同玩乐、抢攻多阶段关卡、与全球玩家竞赛合作。"
-        },
-        {
-          id: 2,
-          url: require("../assets/watermelon.jpg"),
-          name: "合成大西瓜Plus",
-          gameType: "船新版本！",
-          author: "Shinenine",
-          gameUrl: "/static/watermelon/index.html",
-          gameDescription:
-            "2021年最火的休闲小游戏带着它的镜像小游戏一起回来啦！"
-        },
-        {
-          id: 3,
-          url: require("../assets/chazhen.jpg"),
-          name: "新版见缝插针",
-          gameType: "relax",
-          author: "mooneater0",
-          gameUrl: "/static/needle/index.html",
-          gameDescription: "重出江湖 十分轻松休闲的一款小游戏"
-        }
-      ],
-      highest: [
-        {
-          id: 1,
-          url: require("../assets/highest1.png"),
-          name: "原神",
-          gameType: "adventure",
-          author: "Mihayo",
-          gameDescription: "2021最火的RPG游戏"
-        },
-        {
-          id: 2,
-          url: require("../assets/game2.jpg"),
-          name: "王者荣耀",
-          gameType: "action",
-          author: "tencent",
-          gameDescription: "近几年最火的移动端moba类游戏"
-        }
-      ],
-      newest: [
-        {
-          id: 1,
-          url: require("../assets/new1.png"),
-          name: "New",
-          gameType: "action",
-          author: "Shinenine",
-          gameDescription: "什么游戏描述呢"
-        },
-        {
-          id: 2,
-          url: require("../assets/new2.png"),
-          name: "second highest game",
-          gameType: "relax",
-          author: "Shinenine",
-          gameDescription: "描述"
-        }
-      ]
-    };
-  },
-  methods: {
-    imgLinkGame(item) {
-      window.location.href = item.gameUrl;
+  import card from "@/components/card.vue";
+  import navBar from "@/components/navBar.vue";
+  export default {
+    name: "home",
+    components: {
+      navBar,
+      card
     },
-    linkGame(item) {
-      console.log(item.gameUrl);
-      if (item.gameUrl == "#") {
-        Toast("本游戏正在开发中");
-      } else window.location.href = item.gameUrl;
+    data() {
+      return {
+        value: "",
+        pictures: [
+          // 本地路径需要用require包裹
+          {
+            id: 1,
+            gameUrl: "",
+            url: require("../assets/swipe1.png")
+          },
+          {
+            id: 2,
+            url: require("../assets/swipe2.png")
+          },
+          {
+            id: 3,
+            url: require("../assets/swipe3.png")
+          },
+          {
+            id: 4,
+            url: require("../assets/swipe4.png")
+          }
+        ],
+        hottest: [{
+            id: 1,
+            url: require("../assets/angryBird.jpg"),
+            name: "3D愤怒的小鸟",
+            gameType: "最受欢迎的经典益智弹弓小鸟游戏回来了！",
+            author: "Ultrasty",
+            gameUrl: "#",
+            gameDescription: "愤怒小鸟的老玩家？愤怒小鸟经典游戏，你热爱的一切，都还在！而且增添了3D元素。选择要弹出的小鸟，与好友一同玩乐、抢攻多阶段关卡、与全球玩家竞赛合作。"
+          },
+          {
+            id: 2,
+            url: require("../assets/watermelon.jpg"),
+            name: "合成大西瓜Plus",
+            gameType: "船新版本！",
+            author: "Shinenine",
+            gameUrl: "/static/watermelon/index.html",
+            gameDescription: "2021年最火的休闲小游戏带着它的镜像小游戏一起回来啦！"
+          },
+          {
+            id: 3,
+            url: require("../assets/chazhen.jpg"),
+            name: "新版见缝插针",
+            gameType: "relax",
+            author: "mooneater0",
+            gameUrl: "/static/needle/index.html",
+            gameDescription: "重出江湖 十分轻松休闲的一款小游戏"
+          }
+        ],
+        highest: [{
+            id: 1,
+            url: require("../assets/highest1.png"),
+            name: "原神",
+            gameType: "adventure",
+            author: "Mihayo",
+            gameDescription: "2021最火的RPG游戏"
+          },
+          {
+            id: 2,
+            url: require("../assets/game2.jpg"),
+            name: "王者荣耀",
+            gameType: "action",
+            author: "tencent",
+            gameDescription: "近几年最火的移动端moba类游戏"
+          }
+        ],
+        newest: [{
+            id: 1,
+            url: require("../assets/new1.png"),
+            name: "New",
+            gameType: "action",
+            author: "Shinenine",
+            gameDescription: "什么游戏描述呢"
+          },
+          {
+            id: 2,
+            url: require("../assets/new2.png"),
+            name: "second highest game",
+            gameType: "relax",
+            author: "Shinenine",
+            gameDescription: "描述"
+          }
+        ]
+      };
+    },
+    methods: {
+      imgLinkGame(item) {
+        window.location.href = item.gameUrl;
+      },
+      linkGame(item) {
+        console.log(item.gameUrl);
+        if (item.gameUrl == "#") {
+          Toast("本游戏正在开发中");
+        } else window.location.href = item.gameUrl;
+      }
     }
-  }
-};
+  };
+
 </script>
 
 <style>
-.headTitle {
-  float: left;
-  margin-left: 2rem;
-}
+  .headTitle {
+    float: left;
+    margin-left: 2rem;
+  }
 
-.swipe {
-  border-radius: 2rem;
-  border-color: bisque;
-  margin-top: 2rem;
-  height: 13rem;
-  width: 100%;
-  margin-bottom: 2rem;
-}
+  .swipe {
+    border-radius: 2rem;
+    border-color: bisque;
+    margin-top: 2rem;
+    height: 13rem;
+    width: 100%;
+    margin-bottom: 2rem;
+  }
 
-.swipeImg {
-  width: 100%;
-}
+  .swipeImg {
+    width: 100%;
+  }
 
-.tab1 {
-  margin-top: 3rem;
-}
+  .tab1 {
+    margin-top: 3rem;
+  }
 
-.bottomTabs {
-  margin-top: 5rem;
-}
+  .bottomTabs {
+    margin-top: 5rem;
+  }
+
 </style>
